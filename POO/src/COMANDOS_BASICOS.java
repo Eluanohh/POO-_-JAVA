84eaa76b3e6ad659716a3e9ee3852ea9615c9682
Eluan <eluanambrosio3@gmail.com>
2025-04-10 18:52:21 -0300

    Add files via upload

---
+++ POO/src/COMANDOS_BASICOS.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class COMANDOS_BASICOS {
+    static Scanner sc = new Scanner(System.in); // isso aqui e um prompt
+    public static void main(String[] args) {
+        //variaveis de tipos primitivos
+        boolean TrueOrFalse = true;
+        int numeroReais = 0; // 10000000000 a -10000000000
+        double decimais = 0.2; // 10000.200 a -10000.400
+        char letra = 'A';
+
+        // variavel tipo objeto
+        String texto = "Texto";
+        Integer numerosReais2 = 2; // inteiro do tipo objeto
+        Double decimasi2 = 0.2; // decimal do tipo objeto
+
+        String nome = null; // nulo == " "
+        String sexo = " ";
+
+
+        System.out.println("qual seu nome?");
+        nome = sc.nextLine(); // leia - escreva na variavel nome
+
+        System.out.println("o seu nome é " + nome);
+    }
+}
