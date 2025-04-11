e2783fef935f035df26defe112a6456bfedabb2d
Eluanohh <eluanambrosio3@gmail.com>
2025-04-10 21:37:50 -0300

    classeDeObjetos

---
+++ POO/src/Autocarr/Menu.java
@@ -0,0 +1,37 @@
+package Autocarr;
+
+import java.util.Scanner;
+
+public class Menu {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int quantidadeRodas;
+        String chassi;
+        String marca;
+        String tipo;
+
+        Veiculo veiculo = new
+
+                System.out.println("quantidade de rodas: ");
+        veiculo.setQuantidadeRodas(sc.nextInt());
+        System.out.println("chassi: ");
+        veiculo.setChassi(sc.next());
+        System.out.println("marca: ");
+        marca = sc.next();
+        System.out.println("tipo: ");
+        tipo = sc.next();
+
+
+                Veiculo(quantidadeRodas, chassi, marca, tipo);
+        (quantidadeRodas);
+(chassi);
+        veiculo.setMarca(marca);
+        veiculo.setTipo(tipo);
+
+
+
+
+
+        System.out.println(veiculo.toString());
+    }
+}
